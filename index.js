b24abee410496119b61bdc35b8a619ccdbed2a74
Sam Wells <sfwells0518@gmail.com>
2023-07-18 16:17:00 -0500

    set max_tokens to 60 AND for code-runner extension reasons, rename index.jsx file to index.js

--- index.js
+++ index.js
@@ -13,9 +13,9 @@ const openai = new OpenAIApi(configuration);
 document.getElementById("send-btn").addEventListener("click", () => {
   if (setupTextarea.value) {
     const userInput = setupTextarea.value;
-    setupInputContainer.innerHTML = `<img src="public/loading.svg" class="loading" id="loading">`;
+    setupInputContainer.innerHTML = `<img src="/loading.svg" class="loading" id="loading">`;
     movieBossText.innerText = `Ok, just wait a second while my digital brain digests that...`;
-    fetchBotReply();
+    fetchBotReply(userInput);
   }
 });
 
