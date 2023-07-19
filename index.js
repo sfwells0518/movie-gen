d2006a7ee2d6046ecaa5c92af54a686c1637d35c
Sam Wells <sfwells0518@gmail.com>
2023-07-19 11:15:17 -0500

    refactor fetchBotReply and fetchSynopsis

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import { Configuration, OpenAIApi } from "openai";
 
-const setupTextarea = document.getElementById("setup-textarea");
+
 const setupInputContainer = document.getElementById("setup-input-container");
 const movieBossText = document.getElementById("movie-boss-text");
 
@@ -11,6 +11,7 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 
 document.getElementById("send-btn").addEventListener("click", () => {
+  const setupTextarea = document.getElementById("setup-textarea");
   if (setupTextarea.value) {
     const userInput = setupTextarea.value;
     setupInputContainer.innerHTML = `<img src="/loading.svg" class="loading" id="loading">`;
@@ -23,8 +24,26 @@ document.getElementById("send-btn").addEventListener("click", () => {
 async function fetchBotReply(outline) {
   const response = await openai.createCompletion({
     model: "text-davinci-003",
-    prompt: `Generate a short message to enthusiastically say "${outline}" sounds interesting and that you need a few moments to think about it. Mention one aspect of the sentence.`,
-    max_tokens: 60
+    /*
+    1. Refactor this prompt to use examples of an outline and an 
+    enthusiastic response. Be sure to keep the length of your 
+    examples reasonably short, say 20 words or so.
+    */
+    prompt: `Generate a short message to enthusiastically say an outline sounds interesting and that you need a few moments to think about it.
+    ###
+    outline: Two dogs fall in love and move to Hawaii to learn to surf.
+    message: I'll need to think about that. But your idea is amazing! I love the bit about Hawaii!
+    ###
+    outline: A plane crashes in the jungle and the passengers have to walk 1000km to safety.
+    message: I'll spend a few moments considering that. But I love your idea!! A disaster movie in the jungle!
+    ###
+    outline: A group of corrupt lawyers try to send an innocent woman to jail.
+    message: Wow that is awesome! Corrupt lawyers, huh? Give me a few moments to think!
+    ###
+    outline: ${outline}
+    message: 
+    `,
+    max_tokens: 60,
   });
   movieBossText.innerText = response.data.choices[0].text.trim();
 }
@@ -32,8 +51,21 @@ async function fetchBotReply(outline) {
 async function fetchSynopsis(outline) {
   const response = await openai.createCompletion({
     model: "text-davinci-003",
-    prompt: `Generate an engaging, professional, ultra marketable and concise movie synopsis based on the following idea: ${outline}.`,
-    max_tokens: 700
+    /*
+    1. Refactor the prompt to use one or more examples. 
+    2. Remember to separate the instruction from the 
+     example with ### or '''.
+    */
+    prompt: `Generate an engaging, professional and marketable movie synopsis based on an outline.
+    ###
+    outline: A big-headed daredevil fighter pilot goes back to school only to be sent on a deadly mission.
+    synopsis: The Top Gun Naval Fighter Weapons School is where the best of the best train to refine their elite flying skills. When hotshot fighter pilot Maverick (Tom Cruise) is sent to the school, his reckless attitude and cocky demeanor put him at odds with the other pilots, especially the cool and collected Iceman (Val Kilmer). But Maverick isn't only competing to be the top fighter pilot, he's also fighting for the attention of his beautiful flight instructor, Charlotte Blackwood (Kelly McGillis). Maverick gradually earns the respect of his instructors and peers - and also the love of Charlotte, but struggles to balance his personal and professional life. As the pilots prepare for a mission against a foreign enemy, Maverick must confront his own demons and overcome the tragedies rooted deep in his past to become the best fighter pilot and return from the mission triumphant.
+    ###
+    outline: ${outline}
+    synopsis: 
+    `,
+    max_tokens: 700,
+    // potential new prompt: `
   });
   document.getElementById('output-text').innerText = response.data.choices[0].text.trim();
 }
